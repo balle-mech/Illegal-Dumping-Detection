fb5f682ca360e03173bb46db290fe745472143ab
a2c6201 <a2c6201@gmail.com>
2023-09-22 15:27:02 +0900

    chatgptに作成させたコード

--- test.py
+++ test.py
@@ -1,38 +1,43 @@
-import numpy as np
 import cv2
-import tflite_runtime.interpreter as tflite
+import numpy as np
+import tensorflow as tf
 
 # TFLiteモデルのパス
-model_path = '/home/a2c6201/Illegal-Dumping-Detection/object_detection_mobile_object_localizer_v1_1_default_1.tflite'
+# model_path = '/home/a2c6201/Illegal-Dumping-Detection/object_detection_mobile_object_localizer_v1_1_default_1.tflite'
 
-# TFLiteインタープリタの初期化
-interpreter = tflite.Interpreter(model_path=model_path)
+# TensorFlow Liteモデルの読み込み
+interpreter = tf.lite.Interpreter(model_path='/home/a2c6201/Illegal-Dumping-Detection/object_detection_mobile_object_localizer_v1_1_default_1.tflite')
 interpreter.allocate_tensors()
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
 
 # カメラの初期化
-camera = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = camera.read()
+    ret, frame = cap.read()
 
-    if not ret:
-        break
-
-    # フレームをリサイズしてモデルの入力サイズに合わせるなどの前処理を行う
-
-    # モデルに入力を供給
-    input_data = np.array(frame, dtype=np.float32)
-    interpreter.set_tensor(input_details[0]['index'], input_data)
+    # フレームをモデルの入力サイズにリサイズ
+    input_tensor = interpreter.get_input_details()[0]['index']
+    input_shape = interpreter.get_input_details()[0]['shape']
+    resized_frame = cv2.resize(frame, (input_shape[1], input_shape[2]))
+    input_data = np.expand_dims(resized_frame, axis=0)
 
     # 推論を実行
+    interpreter.set_tensor(input_tensor, input_data)
     interpreter.invoke()
 
-    # 出力を取得して解析
-    output_data = interpreter.get_tensor(output_details[0]['index'])
+    # モデルの出力を取得
+    output_tensor = interpreter.get_output_details()[0]['index']
+    output_data = interpreter.get_tensor(output_tensor)
 
-    # 出力を利用して物体認識の結果を描画するなどの後処理を行う
+    # 人物が検出されたら、枠を描画
+    if output_data[0][0] > 0.5:  # 0.5は適切な閾値
+        cv2.rectangle(frame, (0, 0), (frame.shape[1], frame.shape[0]), (0, 255, 0), 2)
 
-camera.release()
+    # フレームを表示
+    cv2.imshow('Person Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
