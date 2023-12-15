a57d4f0a0bc95c858016ff23ce34c6c7ed05da08
a2c6201 <a2c6201@gmail.com>
2023-12-15 17:38:14 +0900

    rm:誤ったpush

--- my_study/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from os.path import join, dirname
-
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-CUSTOM_DATA_PATH = os.environ['CUSTOM_DATA_PATH']
