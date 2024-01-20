26903c8d40f3287f322086992b713f710b31cb45
a2c6201 <a2c6201@gmail.com>
2024-01-20 21:27:00 +0900

    feat:mp4をまとめて命名修正する

---
+++ mmaction2/my_study/data/sort_mp4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+class_num=2
+
+# Set the directory path
+directory="rawvideo_dataset"
+
+# Change to the directory
+cd "$directory" || exit
+
+# Sort the files and rename them
+count=1
+for file in $(ls ${class_num}_* | sort -V); do
+  new_name="${class_num}_${count}.mp4"
+  mv "$file" "${new_name}"
+  ((count++))
+done
