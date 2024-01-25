ad0188f080726ea6fc1311e1c4389305d5ce4dbb
a2c6201 <a2c6201@gmail.com>
2024-01-25 09:12:36 +0900

    feat:echoするの忘れてた

--- mmaction2/my_study/data/convert_mov_to_mp4.sh
+++ mmaction2/my_study/data/convert_mov_to_mp4.sh
@@ -3,8 +3,11 @@
 # rawvideo_datasetディレクトリ内の${クラス番号}_${動画番号}.MOVファイルを指定したコーデックでmp4に変換する
 # ffmpeg -i ./illegal_2.MOV -c:v libx264 -c:a aac -strict experimental -b:a 192k -movflags faststart illegal_2.mp4
 
-class_number=1
-file_number=1
+echo "Enter the class number: "
+read class_num
+
+echo "Enter the file number: "
+read file_num
 
 for file in rawvideo_dataset/*.MOV; do
   ffmpeg -i "$file" -c:v libx264 -c:a aac -strict experimental -b:a 192k -movflags faststart "rawvideo_dataset/${class_number}_${file_number}.mp4"
