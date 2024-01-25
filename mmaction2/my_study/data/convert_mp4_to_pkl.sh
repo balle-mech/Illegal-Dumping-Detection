c897a7f4dc28ea1854daf1e06efbcb3f308b845b
a2c6201 <a2c6201@gmail.com>
2024-01-25 16:36:00 +0900

    feat:指定番号からpklデータ作成できるように

--- mmaction2/my_study/data/convert_mp4_to_pkl.sh
+++ mmaction2/my_study/data/convert_mp4_to_pkl.sh
@@ -6,27 +6,23 @@ read class_num
 echo "Enter the starting video number: "
 read start_num
 
-# Set the directory path
+echo "Enter the data type (train or val): "
+read data_type
+
+# Change to the mmaction2 directory
 directory="../../"
-# Change to the directory
 cd "$directory" || exit
-pwd
 
 # rawvideo_datasetディレクトリ内の${クラス番号}_${train}_${動画番号}.mp4ファイルを指定したコーデックでpklに変換する
 # python ../my_study/ntu_pose_extraction.py ./rawvideo_dataset/${クラス番号}_${train}_${動画番号}.mp4 ./pkl_dataset/${train}/${クラス番号}_${train}_${動画番号}.pkl --skip-postproc
 
-for filepath in $(ls -1 my_study/data/rawvideo_dataset/${class_num}_*.mp4 | sort -V); do
-  file=$(basename $filepath)
-  # echo $file
+for filepath in $(ls -1 my_study/data/rawvideo_dataset/${class_num}_${data_type}_*.mp4 | sort -V); do
+  file_name=$(basename $filepath)
   # .mp4を削除して、代わりに.pklを追加する
-  data_type=$(echo $file | cut -d '_' -f 2)
-  new_name=${file%.mp4}.pkl
-  # echo data_type: ${data_type}
-  # echo new_name: $new_name
+  new_name=${file_name%.mp4}.pkl
 
-  video_num=$(echo $file | cut -d '_' -f 3 | cut -d '.' -f 1)
-  # if [[ $video_num -ge $start_num ]]; then
-  if [[ $data_type -ge 'val' ]]; then
+  video_num=$(echo $file_name | rev | cut -d '_' -f 1 | rev | cut -d '.' -f 1)
+  if [[ $video_num -ge $start_num ]]; then
     echo pkl_dataset/${data_type}/${new_name}を作成しようとしています。
     python my_study/ntu_pose_extraction.py "$filepath" "./my_study/data/pkl_dataset/${data_type}/${new_name}" --skip-postproc
     ((count++))
