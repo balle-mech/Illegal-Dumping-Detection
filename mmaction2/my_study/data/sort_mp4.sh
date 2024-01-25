cc64e649a6a9127fbc72b692abbb252cfe8493ef
a2c6201 <a2c6201@gmail.com>
2024-01-25 16:14:47 +0900

    chore:変数名修正

--- mmaction2/my_study/data/sort_mp4.sh
+++ mmaction2/my_study/data/sort_mp4.sh
@@ -3,16 +3,18 @@
 echo "Enter the class number: "
 read class_num
 
+echo "Enter the start file number: "
+read file_num
+
 # Set the directory path
 directory="rawvideo_dataset"
 
 # Change to the directory
 cd "$directory" || exit
 
-# Sort the files and rename them
-count=1
+# Rename the files
 for file in $(ls ${class_num}_* | sort -V); do
-  new_name="${class_num}_${count}.mp4"
+  new_name="${class_num}_${file_num}.mp4"
   mv "$file" "${new_name}"
-  ((count++))
+  ((file_num++))
 done
