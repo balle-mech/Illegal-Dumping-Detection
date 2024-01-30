dc8809d6a40dec4dc417827e83ece6e2015f5439
a2c6201 <a2c6201@gmail.com>
2024-01-30 16:52:19 +0900

    chore: verbose

--- mmaction2/my_study/config.py
+++ mmaction2/my_study/config.py
@@ -1,9 +1,11 @@
 import os
 from os.path import join, dirname
-
 from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 CUSTOM_DATA_PATH = os.environ['CUSTOM_DATA_PATH']
+RASP_VIDEO_PATH = os.environ['RASP_VIDEO_PATH']
