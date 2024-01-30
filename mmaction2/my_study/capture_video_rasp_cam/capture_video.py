94ca0d56434fa3f80c8f5b183d125c5becc14b0e
a2c6201 <a2c6201@gmail.com>
2024-01-30 16:44:07 +0900

    chore:videopath

--- mmaction2/my_study/capture_video_rasp_cam/capture_video.py
+++ mmaction2/my_study/capture_video_rasp_cam/capture_video.py
@@ -12,6 +12,6 @@ video_path = RASP_VIDEO_PATH + filename
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)  # 解像度を設定
-    camera.start_recording('video.h264')  # h264形式で録画開始
+    camera.start_recording(video_path)  # h264形式で録画開始
     time.sleep(10)  # 10秒間録画
     camera.stop_recording()  # 録画停止
