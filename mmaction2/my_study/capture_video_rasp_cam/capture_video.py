0d12a38c4dc3216dab3efe423976b5c5b1372b70
a2c6201 <a2c6201@gmail.com>
2024-01-30 16:41:20 +0900

    feat:環境変数のインポートエラーを修正

--- mmaction2/my_study/capture_video_rasp_cam/capture_video.py
+++ mmaction2/my_study/capture_video_rasp_cam/capture_video.py
@@ -1,6 +1,14 @@
-# capture_video.py
+# capture_video
+import datetime
 import picamera
 import time
+import config
+
+RASP_VIDEO_PATH = config.RASP_VIDEO_PATH
+
+current_time = datetime.datetime.now()
+filename = '9_' + current_time.strftime("%m%d_%H%M") + '.h264'
+video_path = RASP_VIDEO_PATH + filename
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)  # 解像度を設定
