d98424ab68d90553814b4895c273ec6f542af0d7
a2c6201 <a2c6201@gmail.com>
2024-02-01 11:29:50 +0900

    feat:ソートしたいファイル名を正規表現で変数に格納する処理に

--- mmaction2/my_study/data/sort_mp4.sh
+++ mmaction2/my_study/data/sort_mp4.sh
@@ -7,13 +7,18 @@ echo "Enter the start file number: "
 read file_num
 
 # Set the directory path
-directory="rawvideo_dataset"
+directory="raw_video"
+
+# ソート前のファイル名を正規表現で取得
+unsorted_file_name=${class_num}__[0-9]*.mp4
+# unsorted_file_name=IMG_[0-9]*.mp4
 
 # Change to the directory
 cd "$directory" || exit
 
 # Rename the files
-for file in $(ls ${class_num}_* | sort -V); do
+# for file in $(ls ${class_num}_* | sort -V); do
+for file in $(ls $unsorted_file_name | sort -V); do
   new_name="${class_num}_${file_num}.mp4"
   mv "$file" "${new_name}"
   ((file_num++))
